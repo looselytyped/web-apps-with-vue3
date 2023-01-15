f1915991866b9e4f1a9f7f5f9fb7660db4312cfa
Raju Gandhi <raju.gandhi@gmail.com>
2023-01-15 09:14:37 -0500

    feat: scaffold out the app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,60 @@
-<script setup></script>
-
 <template>
-  <h1>Welcome to the Vue3 Workshop</h1>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <div class="pa-3 text-center bg-teal text-white">
+        <div class="text-h4 pa-4">Friends HQ</div>
+        <p>Together, we are stronger</p>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-list density="compact">
+        <template v-for="(item, i) of items" :key="i">
+          <v-divider dark v-if="item.divider" class="my-3"></v-divider>
+          <v-list-item v-else :value="item">
+            <template v-slot:prepend>
+              <v-icon :icon="item.icon"></v-icon>
+            </template>
+
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-right :color="headerFooterColor" class="text-white">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+    </v-app-bar>
+
+    <v-main>
+      <!-- When using `PeopleList` replace this entire block -->
+      <v-container fluid>
+        <div class="d-flex justify-start mb-6" style="width: 100%">
+          <v-card class="ma-3 pa-3" style="width: 100%">
+            <!-- When using `HelloWorld` use that component here -->
+          </v-card>
+        </div>
+      </v-container>
+    </v-main>
+
+    <v-footer app :color="headerFooterColor" class="text-white" :inset="true">
+      <span>Looselytyped &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref } from "vue";
+
+const headerFooterColor = "light-green lighten-2";
+const items = [
+  { icon: "mdi-view-dashboard", text: "Dashboard" },
+  { icon: "mdi-contacts", text: "Contacts" },
+  { divider: true },
+  { icon: "mdi-clock", text: "Stay tuned!" },
+];
+
+let drawer = ref(null);
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 
-const app = createApp(App);
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+  },
+});
+
+const app = createApp(App).use(vuetify);
 
 app.mount("#app");
