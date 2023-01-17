4a85cdcb21cd2ba88cbbc841f20b9530540cdc91
Raju Gandhi <raju.gandhi@gmail.com>
2023-01-17 10:53:22 -0500

    feat: introduce PersonItem and wire it up with v-for to list friends

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -3,32 +3,12 @@
     <v-row no-gutters class="flex-nowrap">
       <v-card class="d-flex justify-start mx-4" style="width: 100%">
         <v-list header style="width: 100%">
-          <v-list-item v-for="(friend, index) of friends" :key="friend.id">
-            <v-list-item-title class="d-flex justify-start">
-              {{ friend.firstName }}
-              {{ friend.lastName }}
-              <v-spacer />
-
-              <div class="d-flex justify-end">
-                <v-btn density="comfortable" variant="plain" icon>
-                  <v-icon class="edit"> mdi-pencil </v-icon>
-                </v-btn>
-                <v-btn
-                  density="comfortable"
-                  variant="plain"
-                  icon
-                  @click.stop="like(friend)"
-                >
-                  <v-icon class="fav" :color="friend.fav ? 'red' : 'grey'">
-                    mdi-heart
-                  </v-icon>
-                </v-btn>
-              </div>
-            </v-list-item-title>
-
-            <!-- eslint-disable-next-line vue/valid-v-for -->
-            <v-divider v-if="index !== friends.length - 1"></v-divider>
-          </v-list-item>
+          <PersonItem
+            v-for="(friend, index) of friends"
+            :key="friend.id"
+            :friend="friend"
+            :last="index === friends.length - 1"
+          />
         </v-list>
       </v-card>
       <div class="d-flex justify-end mb-6">
@@ -40,6 +20,8 @@
 
 <script setup>
 import { ref } from "vue";
+
+import PersonItem from "@/components/PersonItem.vue";
 
 const friendsMockData = [
   {
---
+++ src/components/PersonItem.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  friend: {
+    type: Object,
+    required: true,
+  },
+  last: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+const like = () => (props.friend.fav = !props.friend.fav);
+</script>
+
+<template>
+  <v-list-item>
+    <v-list-item-title class="d-flex justify-start">
+      {{ friend.firstName }}
+      {{ friend.lastName }}
+      <v-spacer />
+
+      <div class="d-flex justify-end">
+        <v-btn density="comfortable" variant="plain" icon>
+          <v-icon class="edit"> mdi-pencil </v-icon>
+        </v-btn>
+        <v-btn
+          density="comfortable"
+          variant="plain"
+          icon
+          @click.stop="like(friend)"
+        >
+          <v-icon class="fav" :color="friend.fav ? 'red' : 'grey'">
+            mdi-heart
+          </v-icon>
+        </v-btn>
+      </div>
+    </v-list-item-title>
+
+    <v-divider v-if="!last"></v-divider>
+  </v-list-item>
+</template>
