518d0ccacdb2c4d839ca797693b7d362eb2e6e31
Raju Gandhi <raju.gandhi@gmail.com>
2023-02-23 08:00:25 -0500

    build: configure vite to treat all v-* elements as custom elements

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => {
+            return tag.startsWith("v-");
+          },
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
