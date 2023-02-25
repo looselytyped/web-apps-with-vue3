c44f528d9d725bc6e17967faad660ffd4b8847b7
Raju Gandhi <raju.gandhi@gmail.com>
2023-02-25 09:40:55 -0500

    build: configure vite to ignore custom elements in tests

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,44 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => {
-            return tag.startsWith("v-");
-          },
-        },
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
+const vitestConfig = {
+  test: {
+    globals: true,
+    environment: "jsdom",
   },
+};
+
+// https://vitejs.dev/config/
+// prettier-ignore
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      vue(
+        mode === "test"
+          ? {
+            template: {
+              compilerOptions: {
+                isCustomElement: (tag) => {
+                  return tag.startsWith("v-");
+                },
+              },
+            },
+          }
+          : {}
+      ),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    test: vitestConfig.test,
+  };
 });
